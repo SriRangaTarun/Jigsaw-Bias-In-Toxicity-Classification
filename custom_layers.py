32052f28cb24cff754bf954166d3a2bb8c5dd7db
Tarun S Paparaju <tarunp@greenwoodhigh.edu.in>
2020-05-17 12:47:51 +0530

    Update custom_layers.py

--- custom_layers.py
+++ custom_layers.py
@@ -1,3 +1,11 @@
+import keras.backend as K
+from keras.layers import *
+from keras.optimizers import *
+from keras.constraints import *
+from keras.activations import *
+from keras.initializers import *
+from keras.regularizers import *
+
 # https://github.com/bfelbo/DeepMoji/blob/master/deepmoji/attlayer.py
 class AttentionWeightedAverage(Layer):
     def __init__(self, return_attention=False, **kwargs):
@@ -49,3 +57,83 @@ class AttentionWeightedAverage(Layer):
             return None
         else:
             return None * len(input_mask)
+
+class Capsule(Layer):
+    def __init__(self,
+                 num_capsule,
+                 dim_capsule,
+                 routings=3,
+                 share_weights=True,
+                 initializer='glorot_uniform',
+                 activation=None,
+                 regularizer=None,
+                 constraint=None,
+                 **kwargs):
+        super(Capsule, self).__init__(**kwargs)
+        self.num_capsule = num_capsule
+        self.dim_capsule = dim_capsule
+        self.routings = routings
+        self.share_weights = share_weights
+
+        self.activation = activations.get(activation)
+        self.regularizer = regularizers.get(regularizer)
+        self.initializer = initializers.get(initializer)
+        self.constraint = constraints.get(constraint)
+
+    def build(self, input_shape):
+        input_dim_capsule = input_shape[-1]
+        if self.share_weights:
+            self.W = self.add_weight(name='capsule_kernel',
+                                     shape=(1,
+                                            input_dim_capsule,
+                                            self.num_capsule *
+                                            self.dim_capsule),
+                                     initializer=self.initializer,
+                                     regularizer=self.regularizer,
+                                     constraint=self.constraint,
+                                     trainable=True)
+        else:
+            input_num_capsule = input_shape[-2]
+            self.W = self.add_weight(name='capsule_kernel',
+                                     shape=(input_num_capsule,
+                                            input_dim_capsule,
+                                            self.num_capsule *
+                                            self.dim_capsule),
+                                     initializer=self.initializer,
+                                     regularizer=self.regularizer,
+                                     constraint=self.constraint,
+                                     trainable=True)
+
+        self.build = True
+
+    def call(self, inputs):
+        if self.share_weights:
+            u_hat_vectors = K.conv1d(inputs, self.W)
+        else:
+            u_hat_vectors = K.local_conv1d(inputs, self.W, [1], [1])
+
+        batch_size = K.shape(inputs)[0]
+        input_num_capsule = K.shape(inputs)[1]
+        u_hat_vectors = K.reshape(u_hat_vectors, (batch_size,
+                                                  input_num_capsule,
+                                                  self.num_capsule,
+                                                  self.dim_capsule))
+
+        u_hat_vectors = K.permute_dimensions(u_hat_vectors, (0, 2, 1, 3))
+        routing_weights = K.zeros_like(u_hat_vectors[:, :, :, 0])
+
+        for i in range(self.routings):
+            capsule_weights = K.softmax(routing_weights, 1)
+            outputs = K.batch_dot(capsule_weights, u_hat_vectors, [2, 2])
+            if K.ndim(outputs) == 4:
+                outputs = K.sum(outputs, axis=1)
+            if i < self.routings - 1:
+                outputs = K.l2_normalize(outputs, -1)
+                routing_weights = K.batch_dot(outputs, u_hat_vectors, [2, 3])
+                if K.ndim(routing_weights) == 4:
+                    routing_weights = K.sum(routing_weights, axis=1)
+
+        return self.activation(outputs)
+
+    def compute_output_shape(self, input_shape):
+        return (None, self.num_capsule, self.dim_capsule)
